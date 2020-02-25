4af8b48859e43d8075a4fcbcf245fe4dad48aaa3
Ylva Selling <ylva.selling@gmail.com>
2020-02-25 15:08:29 +0100

    Median filtering background modelling implement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,81 @@
 //This line includes all the opencv header files
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>  // Video write
 
 int main() {
-	//Make a blank image
-	cv::Mat picture = cv::Mat::zeros(100, 100, CV_32FC1);
-	//define a rectangle
-	cv::Rect box(30, 30, 25, 25);
+	
+/*    std::string source{"Walk1.mpg"};
+    
+    cv::VideoCapture inputVideo(source);// Open input
+    if (!inputVideo.isOpened())
+    {
+        std::cout  << "Could not open the input video: " << source << std::endl;
+        return -1;
+    }
+    
+    while(1) { */
+    
+        //Load image
+        cv::Mat img0 = cv::imread ("Walk1000.jpg",cv::IMREAD_UNCHANGED);
+        cv::Mat img1 = cv::imread ("Walk1001.jpg",cv::IMREAD_UNCHANGED);
+        cv::Mat walking = cv::imread ("Walk1321.jpg",cv::IMREAD_UNCHANGED);
+        cv::Mat img_medblur{img1};
+        
+    /*    cv::Mat frame;
+        inputVideo >> frame;
+        if(frame.empty()) {
+            break;
+        } */
+        
+        if (!img0.data || !img1.data)
+        {
+            std::cout << "Image not loaded";
+            return -1;
+        }
+        int ksize = 3;
+        
+        cv::medianBlur(img0,img_medblur, ksize);
+        
+        //show the image in the window
+        cv::imshow("img", walking);
+        cv::imshow("img blur", img_medblur);
+        
+        cv::Mat diff;
+        cv::Mat binary;
+        
+        double thresh = 90;
+        absdiff(walking, img_medblur,diff);
+        cv::imshow("diff", diff);
+        //source, destination, color type
+        cv::cvtColor( diff, diff, cv::COLOR_BGR2GRAY);
+        //source, destination, threshold, max value out, threshold type
+        cv::threshold(diff, binary, thresh, 255, cv::ThresholdTypes::THRESH_BINARY);
+        
+        
+        cv::imshow("grayscale binary", binary);
+        int erosion_size = 1;
+        int dilation_size = 2;
+        
+        cv::Mat er_element = cv::getStructuringElement( cv::MORPH_RECT,
+                             cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                             cv::Point( erosion_size, erosion_size ) );
+        
+        cv::erode(binary, binary, er_element);
+        cv::imshow("eroded", binary);
+        
+        cv::Mat dil_element = getStructuringElement( cv::MORPH_RECT,
+                             cv::Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+                             cv::Point( dilation_size, dilation_size ) );
+        
+        cv::dilate(binary, binary, dil_element);
+        cv::imshow("dilated", binary);
 
-	//draw a white rectangle in the image
-	cv::rectangle(picture, box, cv::Scalar(1.0, 1.0, 1.0));
-
-	//create the window that can be resized
-	cv::namedWindow("window");
-
-	//show the image in the window
-	cv::imshow("window", picture);
-
-	//wait until keypress
-	cv::waitKey(0);
+        //wait until keypress
+        cv::waitKey(0);
+   // }
 
 	return 0;
 }
