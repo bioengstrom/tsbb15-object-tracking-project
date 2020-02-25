edfefb61d098a72f1de846c864b470c1de8498bb
Ylva Selling <ylva.selling@gmail.com>
2020-02-25 15:15:16 +0100

    Background modelling for video

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 int main() {
 	
-/*    std::string source{"Walk1.mpg"};
+    std::string source{"Walk1.mpg"};
     
     cv::VideoCapture inputVideo(source);// Open input
     if (!inputVideo.isOpened())
@@ -16,46 +16,49 @@ int main() {
         return -1;
     }
     
-    while(1) { */
+    while(1) {
     
-        //Load image
+     /*   //Load image
         cv::Mat img0 = cv::imread ("Walk1000.jpg",cv::IMREAD_UNCHANGED);
         cv::Mat img1 = cv::imread ("Walk1001.jpg",cv::IMREAD_UNCHANGED);
         cv::Mat walking = cv::imread ("Walk1321.jpg",cv::IMREAD_UNCHANGED);
-        cv::Mat img_medblur{img1};
+        cv::Mat img_medblur{img1}; */
+        cv::Mat background = cv::imread ("Walk1321.jpg",cv::IMREAD_UNCHANGED);
         
-    /*    cv::Mat frame;
+        cv::Mat frame;
         inputVideo >> frame;
         if(frame.empty()) {
             break;
-        } */
+        }
         
+  /*
         if (!img0.data || !img1.data)
         {
             std::cout << "Image not loaded";
             return -1;
-        }
+        } */
         int ksize = 3;
         
-        cv::medianBlur(img0,img_medblur, ksize);
+        cv::medianBlur(background,background, ksize);
         
         //show the image in the window
-        cv::imshow("img", walking);
-        cv::imshow("img blur", img_medblur);
+       // cv::imshow("img", walking);
+        //cv::imshow("img blur", img_medblur);
         
         cv::Mat diff;
         cv::Mat binary;
         
         double thresh = 90;
-        absdiff(walking, img_medblur,diff);
+        absdiff(frame, background, diff);
         cv::imshow("diff", diff);
+        //Make grayscale
         //source, destination, color type
         cv::cvtColor( diff, diff, cv::COLOR_BGR2GRAY);
+        //Threshold
         //source, destination, threshold, max value out, threshold type
         cv::threshold(diff, binary, thresh, 255, cv::ThresholdTypes::THRESH_BINARY);
         
-        
-        cv::imshow("grayscale binary", binary);
+        //cv::imshow("grayscale binary", binary);
         int erosion_size = 1;
         int dilation_size = 2;
         
@@ -64,7 +67,7 @@ int main() {
                              cv::Point( erosion_size, erosion_size ) );
         
         cv::erode(binary, binary, er_element);
-        cv::imshow("eroded", binary);
+        //cv::imshow("eroded", binary);
         
         cv::Mat dil_element = getStructuringElement( cv::MORPH_RECT,
                              cv::Size( 2*dilation_size + 1, 2*dilation_size+1 ),
@@ -74,8 +77,10 @@ int main() {
         cv::imshow("dilated", binary);
 
         //wait until keypress
-        cv::waitKey(0);
-   // }
+        char c = (char)cv::waitKey(25);
+        if(c==27)
+            break;
+    }
 
 	return 0;
 }
