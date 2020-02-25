06bed9d3ae91f96f855fb27435b4bf18fb223e17
Ylva Selling <ylva.selling@gmail.com>
2020-02-25 17:05:37 +0100

    Add harris tracking funtion

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,37 @@ cv::Mat medianBackgroundModelling(cv::Mat frame, cv::Mat background, int ksize =
     
     return binary;
 }
+/*
+ Harris features
+ Source code: https://docs.opencv.org/3.4/d4/d7d/tutorial_harris_detector.html
+ */
+void cornerHarris_demo(cv::Mat src, cv::Mat src_gray)
+{
+    int blockSize = 2;
+    int apertureSize = 3;
+    double k = 0.04;
+    int thresh = 200;
+    int max_thresh = 255;
+    
+    cv::Mat dst = cv::Mat::zeros( src.size(), CV_32FC1 );
+    cornerHarris( src_gray, dst, blockSize, apertureSize, k );
+    
+    cv::Mat dst_norm, dst_norm_scaled;
+    cv::normalize( dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat() );
+    cv::convertScaleAbs( dst_norm, dst_norm_scaled );
+    for( int i = 0; i < dst_norm.rows ; i++ )
+    {
+        for( int j = 0; j < dst_norm.cols; j++ )
+        {
+            if( (int) dst_norm.at<float>(i,j) > thresh )
+            {
+                circle( dst_norm_scaled, cv::Point(j,i), 5,  cv::Scalar(0), 2, 8, 0 );
+            }
+        }
+    }
+    cv::namedWindow( "Harris features" );
+    cv::imshow("Harris features", dst_norm_scaled );
+}
 
 int main() {
 	
@@ -70,6 +101,15 @@ int main() {
         bg_mask = medianBackgroundModelling(frame, background, 3, threshold, 1, 1);
         cv::imshow("Original video", frame);
         cv::imshow("Background model", bg_mask);
+        //Make grayscale
+        //source, destination, color type
+        cv::Mat grayFrame;
+        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
+        cornerHarris_demo(frame, grayFrame);
+        
+        /*
+         TODO: use the cv::goodfunctionstotrack() function
+         */
 
         //Break if press ESC
         char c = (char)cv::waitKey(25);
