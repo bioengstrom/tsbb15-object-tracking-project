66c1b50431989f8a0af3a9a07982a96942e5e8f4
Jonas <jonas.langdahl97@gmail.com>
2020-02-25 16:27:49 +0100

    skitgit
    Copied master code into branch

--- src/main.cpp
+++ src/main.cpp
@@ -6,70 +6,76 @@
 #include <opencv2/highgui/highgui.hpp>  // Video write
 
 cv::Mat medianBackgroundModelling(cv::Mat frame, cv::Mat background, int ksize = 3, double thresh = 90, int erosion_size = 1, int dilation_size = 2) {
-    
+
     cv::Mat diff;
     cv::Mat binary;
-    cv::medianBlur(background,background, ksize);
+    cv::medianBlur(background, background, ksize);
 
     absdiff(frame, background, diff);
     //Make grayscale
     //source, destination, color type
-    cv::cvtColor( diff, diff, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(diff, diff, cv::COLOR_BGR2GRAY);
     //Threshold
     //source, destination, threshold, max value out, threshold type
-    cv::threshold(diff, binary, thresh, 255, cv::ThresholdTypes::THRESH_BINARY);
+    cv::threshold(diff, binary, thresh, 1.0, cv::ThresholdTypes::THRESH_BINARY);
 
-    cv::Mat er_element = cv::getStructuringElement( cv::MORPH_RECT,
-                         cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                         cv::Point( erosion_size, erosion_size ) );
-    
+    cv::Mat er_element = cv::getStructuringElement(cv::MORPH_RECT,
+        cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+        cv::Point(erosion_size, erosion_size));
+
     cv::erode(binary, binary, er_element);
-    
-    cv::Mat dil_element = getStructuringElement( cv::MORPH_RECT,
-                         cv::Size( 2*dilation_size + 1, 2*dilation_size+1 ),
-                         cv::Point( dilation_size, dilation_size ) );
-    
+
+    cv::Mat dil_element = getStructuringElement(cv::MORPH_RECT,
+        cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+        cv::Point(dilation_size, dilation_size));
+
     cv::dilate(binary, binary, dil_element);
-    
+
+    //std::cout << binary << std::endl;
+    binary.convertTo(binary, CV_32FC1);
+
     return binary;
 }
 
 int main() {
-	
+
     //Name of video
-	//kek
-    std::string source{"Walk1.mpg"};
-    
+    std::string source{ "Walk1.mpg" };
+
     cv::VideoCapture inputVideo(source); // Open input
     if (!inputVideo.isOpened())
     {
-        std::cout  << "Could not open the input video: " << source << std::endl;
+        std::cout << "Could not open the input video: " << source << std::endl;
         return -1;
     }
-    
-    while(1) {
-        
-        cv::Mat background = cv::imread ("Walk1000.jpg",cv::IMREAD_UNCHANGED);
-        if(background.empty()) {
+
+    int threshold = 90;
+    cv::namedWindow("Threshold", cv::WINDOW_AUTOSIZE);
+    cv::createTrackbar("Threshold", "Threshold", &threshold, 255);
+
+    while (1) {
+
+        cv::Mat background = cv::imread("Walk1000.jpg", cv::IMREAD_UNCHANGED);
+        if (background.empty()) {
             return 1;
         }
         cv::Mat frame;
         cv::Mat bg_mask;
-        
+
         inputVideo >> frame;
-        if(frame.empty()) {
+        if (frame.empty()) {
             break;
         }
         //frame, background, ksize for median filter, threshold, erosion size, dilation size
-        bg_mask = medianBackgroundModelling(frame, background, 3, 90, 1, 1);
+        bg_mask = medianBackgroundModelling(frame, background, 3, threshold, 1, 1);
         cv::imshow("Original video", frame);
         cv::imshow("Background model", bg_mask);
 
         //Break if press ESC
         char c = (char)cv::waitKey(25);
-        if(c==27)
+        if (c == 27)
             break;
     }
 
-	return 0;
+    return 0;
 }
