3af80b21655b16d0c201c1eaf79a2f010171c7ae
Jonas <jonas.langdahl97@gmail.com>
2020-02-26 11:47:10 +0100

    Some implementation
    dunno, this sucks so much

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,34 @@ cv::Mat medianBackgroundModelling(cv::Mat frame, cv::Mat background, int ksize =
     return binary;
 }
 
+cv::Mat mixtureBackgroundModelling(cv::Mat frame, int K, int D, double alpha, double T, double w_init, double covar_init) {
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+    cv::Size s = frame.size();
+    double d; //mahalanobis
+    double x = 0; //current pixel val
+    double mean;
+    cv::Vec<double,4> covar = covar_init;
+    // for each pixel in frame[row,col]
+    for (int row = 0; row < s.height; row++) {
+        for (int col = 0; col < s.width; col++) {
+            
+            x = frame.at<double>(row, col);
+            mean = x;
+            //var = (x - mean) * (x - mean);
+
+            // for all mixture models
+            for (int k = 0; k < K; k++) {
+                
+                //d = cv::Mahalanobis(x, mean, var);
+
+            }
+        }
+
+        
+    }
+    
+}
+
 int main() {
 
     //Name of video
@@ -67,9 +95,10 @@ int main() {
             break;
         }
         //frame, background, ksize for median filter, threshold, erosion size, dilation size
-        bg_mask = medianBackgroundModelling(frame, background, 3, threshold, 1, 1);
+        //bg_mask = medianBackgroundModelling(frame, background, 3, threshold, 1, 1);
+        
         cv::imshow("Original video", frame);
-        cv::imshow("Background model", bg_mask);
+        //cv::imshow("Background model", bg_mask);
 
         //Break if press ESC
         char c = (char)cv::waitKey(25);
