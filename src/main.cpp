cf70d0b40bbde0a21e6c3e656d4ec9c283e86e13
Ylva Selling <ylva.selling@gmail.com>
2020-02-28 13:33:27 +0100

    Add a simple version of maintaining object identity

--- src/main.cpp
+++ src/main.cpp
@@ -187,15 +187,27 @@ int main() {
     bool tracking_started = false;
     float fps{};
     
+    /**********************************************************************
+              MAINTAIN OBJECT IDENTITY
+    
+    ************************************************************************/
+    
+    std::vector<cv::Rect> unique_objects;
+    std::vector<cv::Scalar> unique_colors;
+    
     
     
     /**************************************************************************
             SLIDER FOR ADJUSTING THRESHOLD
     ***************************************************************************/
-    int threshold = 90;
-    int threshold_slider_max = 255;
+    int threshold{85};
+    int threshold_slider_max{255};
+    int erosion_size{1};
+    int dilation_size{9};
     cv::namedWindow("Image", cv::WINDOW_NORMAL);
     cv::createTrackbar("Threshold", "Image", &threshold, threshold_slider_max);
+    cv::createTrackbar("Erosion", "Image", &erosion_size, 10);
+    cv::createTrackbar("Dilation", "Image", &dilation_size, 10);
   
     while(1) {
         
@@ -219,7 +231,84 @@ int main() {
                    BACKGROUND MODELLING
         ***************************************************************************/
         //frame, background, ksize for median filter, threshold, erosion size, dilation size
-        cv::Mat bg_mask = medianBackgroundModelling(frame, background, 3, threshold, 1, 1);
+        cv::Mat bg_mask = medianBackgroundModelling(frame, background, 3, threshold, erosion_size, dilation_size);
+        
+        /**************************************************************************
+                   BOUNDING BOXES
+            source: https://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/bounding_rects_circles/bounding_rects_circles.html
+        ***************************************************************************/
+        
+        std::vector<std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+        
+        // Find contours
+        cv::findContours( bg_mask, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+        
+        // Approximate contours to polygons + get bounding rects and circles
+        std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+        std::vector<cv::Rect> boundRect( contours.size() );
+        //std::vector<cv::Point2f>center( contours.size() );
+        //std::vector<float>radius( contours.size() );
+
+        for( int i = 0; i < contours.size(); i++ )
+           {
+             approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
+             boundRect[i] = boundingRect( cv::Mat(contours_poly[i]) );
+             //minEnclosingCircle( (cv::Mat)contours_poly[i], center[i], radius[i] );
+           }
+
+
+        // Draw polygonal contour + bonding rects + circles
+      cv::Mat drawing = cv::Mat::zeros( bg_mask.size(), CV_8UC3 );
+         
+        for( int i = 0; i< contours.size(); i++ )
+           {
+             cv::Scalar color = cv::Scalar( 0, 255, 255);
+             //drawContours( bg_mask, contours_poly, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+             rectangle( frame, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+             //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+           }
+         
+        /**************************************************************************
+                   DETECT OVERLAP
+        ***************************************************************************/
+        
+        int overlap_area{0};
+        int new_overlap{0};
+        bool found_obj{false};
+    
+        //Greedy algorithm, assign the rectangle with most overlap to the unique object
+        for(int i{0}; i < boundRect.size(); i++) {
+            for(int j{0}; j < unique_objects.size(); j++) {
+                //Check intersection between areas and assign bounding rectangle to last rectangle
+                new_overlap = (unique_objects[j] & boundRect[i]).area();
+                if(overlap_area < new_overlap) {
+                    std::cout << "Found overlap" << std::endl;
+                    overlap_area = new_overlap;
+                    //Update to new position
+                    unique_objects[j] = cv::Rect{boundRect[i]};
+                    found_obj = true;
+                }
+            }
+            if(!found_obj) {
+                std::cout << "Added new object" << std::endl;
+                unique_objects.push_back(cv::Rect{boundRect[i]});
+                unique_colors.push_back(cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255)));
+            }
+            else {
+                std::cout << "Assigned to object" << std::endl;
+            }
+            found_obj = false;
+            overlap_area = 0;
+            
+        }
+        std::cout << "Found objects: " << unique_objects.size() << std::endl << std::endl;
+        
+        // Draw unique object bonding rects
+        for( int i = 0; i< unique_objects.size(); i++ )
+           {
+             rectangle( drawing, unique_objects[i].tl(), unique_objects[i].br(), unique_colors[i], 2, 8, 0 );
+           }
         
         /**************************************************************************
                     HARRIS FEATURE POINTS
@@ -227,6 +316,7 @@ int main() {
         //Find good feature points to track
         //Make grayscale
         //source, destination, color type
+        
         cv::Mat grayFrame;
         cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
         //cv::Mat harris;
@@ -261,7 +351,6 @@ int main() {
                 circle( displayFeatures, corners[1][i], 5, cv::Scalar(255, 255, 255), 2, 8, 0);
                 //Push rectangle surronding feature point to vector
                 bboxes.push_back(cv::Rect(corners[1][i].x, corners[1][i].x, 20, 20));
-                std::cout << corners[1][i].x << " " << corners[1][i].x << std::endl;
             }
             if(bboxes.size() >= enough_feature_points) {
                 std::cout << "Found enough feature points: " << bboxes.size() << std::endl;
@@ -279,7 +368,6 @@ int main() {
             for(int i=0; i < bboxes.size(); i++) {
                 //Make sure the bounding box is comletely inside the frame
                 if(0 <= bboxes[i].x && 0 <= bboxes[i].width && bboxes[i].x + bboxes[i].width <= tracking_frame.cols && 0 <=  bboxes[i].y && 0 <=  bboxes[i].height &&  bboxes[i].y +  bboxes[i].height <= tracking_frame.rows) {
-                    std::cout << "Initialized tracking at point: " << bboxes[i] << std::endl;
                     colors.push_back(cv::Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255)));
                     multiTracker->add(cv::TrackerBoosting::create(), tracking_frame, cv::Rect2d(bboxes[i]));
                 }
@@ -312,12 +400,13 @@ int main() {
         ***************************************************************************/
         //Display the threshold as text on the mask
         //(Mat& img, const string& text, Point org, int fontFace, double fontScale, Scalar color, int thickness=1, int lineType=8, bool bottomLeftOrigin=false )
-        cv::putText(frame, "Original Frame", cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
+        cv::putText(frame, "Original Frame & bounding boxes", cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
         cv::putText(bg_mask, "Background modelling", cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
         cv::putText(bg_mask, "Threshold: " + std::to_string(threshold), cv::Point(10,60), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
-        cv::putText(displayFeatures, "Harris feature points" , cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
+        //cv::putText(displayFeatures, "Harris feature points" , cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
+        cv::putText(drawing, "Dectected objects: " + std::to_string(unique_objects.size()), cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255, 255, 255),2);
         //cv::putText(harris, "Harris feature points" , cv::Point(10,30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 0),2);
-        ShowFourImages("Image", frame, display(bg_mask), display(displayFeatures), tracking_frame);
+        ShowFourImages("Image", frame, display(bg_mask), drawing, tracking_frame);
         //Break if press ESC
         char c = (char)cv::waitKey(25);
         if(c==27)
