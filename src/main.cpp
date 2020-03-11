f4a7b752675bf5a899f30f048581a7df6504d909
unknown <danielsahlin8@gmail.com>
2020-03-11 16:39:41 +0100

    testsetetst

--- src/main.cpp
+++ src/main.cpp
@@ -49,36 +49,48 @@ struct SortStruct
     double covar;
 };
 
-double isForeground(double x, double w_init, int K = 5, double alpha = 0.002, double T = 0.8, double var_init = 10.0)  {
+struct mix_comp
+{
+    mix_comp() {
+        covar = 10.0;
+        my = 10.0;
+        w = 0.002;
+    };
+
+    double covar;
+    double my;
+    double w;
+   
+
+};
+
+double isForeground(double x, std::vector<mix_comp> &mix_comps,double w_init, int K = 5, double alpha = 0.002, double T = 0.8, double var_init = 10.0)  {
 
     std::vector<SortStruct> variables{};
     const double lambda = 2.5;
     bool match{false};
     int m{};
     double totWeight = 0.0;
-
+    
 
     std::vector<double> d(K, 0.0); //mahalanobis
     std::vector<double> covar_init(K, var_init);
-    std::vector<double> covar(K, var_init);
-    std::vector<double> my(K, 15.0);
-    std::vector<double> w(K, w_init);
     std::vector<double> rho(K, 0.0);
     std::vector<double> quotient_vec(K, 0.0);
-
 
     // For all k mixture models, check if the pixel is foreground or background
     // Match == true means the pixel value is matching the mixture model
     for (int k = 0; k < K; k++) {
 
-        d[k] = sqrt((x - my[k]) * (x - my[k]));
+        d[k] = sqrt((x - mix_comps[k].my) * (x - mix_comps[k].my));
+        
 
-        if (d[k] < lambda * sqrt(covar[k])) {
+        if (d[k] < lambda * sqrt(mix_comps[k].covar)) {
 
             if (match == false) {
                 m = k;
             }
-            else if ((w[k] / sqrt(covar[k])) > (w[m] / sqrt(covar[m]))) {
+            else if ((mix_comps[k].w / sqrt(mix_comps[k].covar)) > (mix_comps[m].w / sqrt(mix_comps[k].covar))) {
                 m = k;
             }
             match = true;
@@ -87,41 +99,44 @@ double isForeground(double x, double w_init, int K = 5, double alpha = 0.002, do
 
 	if (match == false) {
 		m = K;
-		w[m] = alpha;
-		my[m] = x;
-		covar[m] = covar_init[m];
+        mix_comps[m].w = alpha;
+        mix_comps[m].my = x;
+		mix_comps[m].covar = covar_init[m];
 	}
 	else {
-		w[m] = ((1 - alpha) * w[m]) + alpha;
-		rho[m] = alpha / w[m];
-		my[m] = ((1 - rho[m]) * my[m]) + (rho[m] * x);
-		covar[m] = ((1 - rho[m]) * covar[m]) + (rho[m] * (x - my[m]) * (x - my[m]));
+        mix_comps[m].w = ((1 - alpha) * mix_comps[m].w) + alpha;
+		rho[m] = alpha / mix_comps[m].w;
+        mix_comps[m].my = ((1 - rho[m]) * mix_comps[m].my) + (rho[m] * x);
+		mix_comps[m].covar = ((1 - rho[m]) * mix_comps[m].covar) + (rho[m] * (x - mix_comps[m].my) * (x - mix_comps[m].my));
 	}
         
 	// RENORMALIZE W
 	totWeight = 0;
-	for (auto& n : w)
-		totWeight += n;
+    for (int i = 0; i < K; i++)
+    {
+        totWeight += mix_comps[i].w;
+    }
+		
 
 	for (int i = 0; i < K; i++)
 	{
-		w[i] = w[i] / totWeight;
+        mix_comps[i].w = mix_comps[i].w / totWeight;
 	}
 
 	if (match) {
 
 		// Sort w, my, covar with respect to weight/covariance ratio
 		for (int i = 0; i < K; i++) {
-			quotient_vec[i] = w[i] / sqrt(covar[i]);
-			variables.push_back({ quotient_vec[i], w[i], my[i], covar[i] });
+			quotient_vec[i] = mix_comps[i].w / sqrt(mix_comps[i].covar);
+			variables.push_back({ quotient_vec[i], mix_comps[i].w, mix_comps[i].my, mix_comps[i].covar});
 		}
 		std::sort(variables.begin(), variables.end(),
 			[](const SortStruct i, const SortStruct j) {return i.quotient < j.quotient; });
 
 		for (int i = 0; i < K; i++) {
-			w[i] = variables[i].w;
-			my[i] = variables[i].my;
-			covar[i] = variables[i].covar;
+            mix_comps[i].w = variables[i].w;
+            mix_comps[i].my = variables[i].my;
+			mix_comps[i].covar = variables[i].covar;
 		}
 	}
 
@@ -129,7 +144,7 @@ double isForeground(double x, double w_init, int K = 5, double alpha = 0.002, do
 	int B = 0;
 			   
 	for (int k = 0; k < K; k++) {
-		sum += w[k];
+		sum += mix_comps[k].w;
 		if (sum > T) {
 			B = k;
 			break;
@@ -138,20 +153,20 @@ double isForeground(double x, double w_init, int K = 5, double alpha = 0.002, do
 	//Background segmentation for gaussian mixture model
 	for (int k = 0; k < B; k++)
 	{
-		d[k] = sqrt((x - my[k]) * (x - my[k]));
-		if (d[k] < lambda * sqrt(covar[k])) {
+		d[k] = sqrt((x - mix_comps[k].my) * (x - mix_comps[k].my));
+		if (d[k] < lambda * sqrt(mix_comps[k].covar)) {
 			return 1.0;
 		}
 	}
     return 0.0;
 }
 
-void mixtureBackgroundModelling(cv::Mat &frame, double w_init, double var_init, int K = 5, double alpha = 0.002, double T = 0.8) {
+void mixtureBackgroundModelling(cv::Mat &frame, cv::Mat &mix_comps,double w_init, double var_init, int K = 5, double alpha = 0.002, double T = 0.8) {
 
     cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
     frame.convertTo(frame, CV_64F);
-    frame.forEach<double>([&] (double &x, const int * position) -> void {
-        x = isForeground(x, w_init, K, alpha, T, var_init);
+    frame.forEach<double>([&] (double &x, const int *position) -> void {
+        x = isForeground(x, mix_comps.at<std::vector<mix_comp>>(position),w_init, K, alpha, T, var_init);
     });
 }
 
@@ -165,24 +180,35 @@ int main() {
         std::cout << "Could not open the input video: " << source << std::endl;
         return -1;
     }
-
-    int threshold = 10;
     cv::Mat frame;
+    inputVideo >> frame;
+  
+    int threshold = 10;
+    
+    //std::vector<std::vector<mix_comp>> mix_comps(frame.rows, std::vector<mix_comp>(frame.cols));
+    //mix_comp lol = mix_comps[0][0];
+    std::vector<mix_comp> kok(5);
+    cv::Mat mix_comps(kok,frame.size);
+   
+
+    //cv::Mat<std::vector<mix_comp>> kek;
 
     while (1) {
 
-        inputVideo >> frame;
-        if (frame.empty()) {
-            break;
-        }
+        
         //(cv::Mat &frame, double w_init, double var_init, int K = 5, double alpha = 0.002, double T = 0.8)
-        mixtureBackgroundModelling(frame, 0.002, 10.0, 5, 0.002, 0.8);
+        mixtureBackgroundModelling(frame, mix_comps,0.002, 10.0, 5, 0.002, 0.8);
         cv::imshow("Original video", frame);
         
         //Break if press ESC
         char c = (char)cv::waitKey(25);
         if (c == 27)
             break;
+
+        inputVideo >> frame;
+        if (frame.empty()) {
+            break;
+        }
     }
 
     return 0;
